5cbb46133ed9f8db17a2d6e6bdc5e62ab875b29a
David Foote <david.a.foote@gmail.com>
2019-07-15 23:32:34 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,11 +6,11 @@ class Backer
     @name = name
   end
   
-  def back_project(project)
-    @backed_projects << project
+  def back_project(project, backer)
+    @backed_projects << project, backer
   end
   
-  def more_advanced_back_project(backer)
-    @backed_projects << backer
-  end
+  # def more_advanced_back_project(backer)
+  #   @backed_projects << backer
+  # end
 end
